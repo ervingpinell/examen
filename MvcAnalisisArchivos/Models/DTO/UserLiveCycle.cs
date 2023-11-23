ead4768560fa64bb80f2f087ce6e408299105bea
Elias <eliassaso@gmail.com>
2023-11-22 19:37:46 -0600

    Implementación de html puro y creación de nuevo DTO

---
+++ MvcAnalisisArchivos/Models/DTO/UserLiveCycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAnalisisArchivos.Models.DTO
+{
+    public class UserLiveCycle
+    {
+        public UserDTO User { get; set; }
+       // public AtmParameterDTO atm { get; set; }
+    }
+}
