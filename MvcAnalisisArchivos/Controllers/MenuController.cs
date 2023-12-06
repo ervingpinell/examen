18e32a3d1a86e9526da3e25c4b9ef30ecd2a064c
Erving <ervingpinell@gmail.com>
2023-12-06 17:43:16 -0600

    Fixes

--- MvcAnalisisArchivos/Controllers/MenuController.cs
+++ MvcAnalisisArchivos/Controllers/MenuController.cs
@@ -9,34 +9,104 @@ using System.Net;
 using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
+using static System.Collections.Specialized.BitVector32;
 
 namespace MvcAnalisisArchivos.Controllers
 {
     public class MenuController : Controller
     {
-         UserDAO userDAO = new UserDAO();
-        AtmParameterDAO atmDAO = new AtmParameterDAO();
-        UserLiveCycleDTO liveSession;
+        private AtmParameterDAO atmDAO = new AtmParameterDAO();
+        private UserDTO userDTO = new UserDTO();
+        private UserDAO userDAO = new UserDAO();
+        private UserLiveCycleDTO liveSession = new UserLiveCycleDTO();
+ 
 
         // GET: Menu
-        public ActionResult mainMenu (string session)
+        public ActionResult mainMenu(UserLiveCycleDTO liveSession)
         {
-            string safetyDecodedObject = WebUtility.UrlDecode(session);
-            var liveSession = JsonConvert.DeserializeObject<UserLiveCycleDTO>(safetyDecodedObject);
-            return View(liveSession);
+            UserLiveCycleDTO session = TempData["liveSession"] as UserLiveCycleDTO;
+            return View(session);
         }
         public ActionResult showBalance(UserLiveCycleDTO liveSession)
         {
-            return View(liveSession);
+            UserLiveCycleDTO session = TempData["liveSession"] as UserLiveCycleDTO;
+
+            return View(session);
         }
 
         public ActionResult withdrawMoney(UserLiveCycleDTO liveSession)
         {
-            return View(liveSession);
+            UserLiveCycleDTO session = TempData["liveSession"] as UserLiveCycleDTO;
+
+            return View(session);
+        }
+        // POST: menu/withDraw
+        [HttpPost]
+        public ActionResult withdrawMoney(int mount, UserLiveCycleDTO liveSession, UserDAO userDAO, UserDTO userDTO)
+        {
+            UserLiveCycleDTO session = TempData["liveSession"] as UserLiveCycleDTO;
+            
+            int amount = 0;
+            userDTO = session.User;
+            while (userDTO.Balance > 0)
+            {
+                if (userDTO.Balance > amount)
+                {
+                    userDTO.Balance = userDTO.Balance - amount;
+                    
+                    userDAO.UpdateUser(userDTO.Balance, userDTO);
+
+                    Console.WriteLine("Haz hecho un retiro de: " + amount + "\nSaldo en cuenta: " + userDTO.Balance);
+
+                    session.User = userDTO;
+                    return View(session);
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente");
+
+                }
+
+            }
+            return View(session);
         }
 
         public ActionResult exit()
         {
+
+            return View();
+        }
+
+        public ActionResult logIn()
+        {
+            return View();
+        }
+
+        
+        // POST: menu/LogIn
+        [HttpPost]
+        public ActionResult logIn(string email, string code)
+        {
+            try
+            {
+                userDTO = userDAO.ReadUser(email);
+
+                if (userDTO.VerifyCode(code, userDTO.Code))
+                {
+
+                    liveSession.User = userDTO;
+                    liveSession.Atm = atmDAO.ReadAtm(1);
+                    liveSession.Session = 1;
+                    liveSession.Id = 1;
+                    TempData["liveSession"] = liveSession;
+                    return View("mainMenu",liveSession);
+
+                }
+            }
+            catch
+            {
+                return View();
+            }
             return View();
         }
     }
--- MvcAnalisisArchivos/Models/DTO/UserLiveCycleDTO.cs
+++ MvcAnalisisArchivos/Models/DTO/UserLiveCycleDTO.cs
@@ -7,6 +7,7 @@ namespace MvcAnalisisArchivos.Models.DTO
 {
     public class UserLiveCycleDTO
     {
+       public int Id { get; set;}
        public int Session { get; set; }
        public UserDTO User { get; set; }
        public AtmParameterDTO Atm { get; set; }
